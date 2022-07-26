a95f2cdc20066e804ed022a55b48ffabf4a3cdf8
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-07-26 13:43:54 +0100

    Feature: project init and added header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+ import React from 'react';
+
+ import mealsImg from '../../assets/meals.jpg';
+ import classes from './Header.module.css';
+ 
+ export default function Header() {
+   return (
+     <>
+      <header className={classes.header}>
+        <h1>Just Munch</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImg} alt='table of food'/>
+      </div>
+     </>
+   )
+ }
+ 
