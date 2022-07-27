7ae0dc74c0dd7a05bf99b005e9471107c5a1a070
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-07-27 12:15:03 +0100

    Feature: cart using context

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import classes from './CartButton.module.css';
-import CartIcon from '../Cart/CartIcon'
+import classes from "./CartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 export default function CartButton(props) {
+  const ctx = useContext(CartContext);
+
+  const numberOfItems = ctx.items.reduce((currentValue, item) => {
+    return currentValue + item.amount;
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
-  )
+  );
 }
