fb0fd982bac3d4c3e25a6ff5ebf0268439b5962b
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-07-27 16:40:32 +0100

    Feature: cart now bumps on add item

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,18 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import classes from "./CartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 export default function CartButton(props) {
+  const [highlightBtn, setHighlightBtn] = useState(false);
   const ctx = useContext(CartContext);
+  const { items } = ctx;
 
-  const numberOfItems = ctx.items.reduce((currentValue, item) => {
+  const numberOfItems = items.reduce((currentValue, item) => {
     return currentValue + item.amount;
-  }, 0)
+  }, 0);
+
+  const btnClasses = `${classes.button} ${highlightBtn ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setHighlightBtn(true);
+    const timer = setTimeout(() => {
+      setHighlightBtn(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
